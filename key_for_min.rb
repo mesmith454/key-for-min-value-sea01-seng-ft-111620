0a6b13a61fd2d969e6c87ae4f3c23a8d636682b9
vivacious-revert-7013 <mesmith454@gmail.com>
2020-11-05 20:12:56 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,10 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+  min = 50
+  name_hash.each do |key, value|
+    if hash[key][value] < min
+      min = hash[key][value]
+    end
+    min
 end
